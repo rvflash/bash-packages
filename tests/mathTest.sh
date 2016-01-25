4f92fb717cd385a24e0de2103f0d5f152af95767
hgouchet <herve.gouchet@twenga.com>
2016-01-25 15:55:48 +0100

    Add int, decimal, floor and rand methods to number package

--- tests/mathTest.sh
+++ tests/mathTest.sh
@@ -7,10 +7,79 @@ source ../math.sh
 declare -i -r TEST_NUMBER_TYPE_INT=127
 readonly TEST_NUMBER_STR_INT="12"
 readonly TEST_NUMBER_STR_BIG_INT="13"
+readonly TEST_NUMBER_STR_FLOAT_LEADING_ZERO="5.012"
+readonly TEST_NUMBER_STR_INT_LEADING_ZERO="012"
 readonly TEST_NUMBER_STR_FLOAT="12.45"
 readonly TEST_NUMBER_STR_BIG_FLOAT="12.55"
-readonly TEST_NUMBER_STR_BIGGER_FLOAT="13.45"
+readonly TEST_NUMBER_STR_BIGGER_FLOAT="13.12"
 readonly TEST_NUMBER_STR_UNKNOWN="12s"
+
+
+readonly TEST_DECIMAL="-11-11-11-01-01"
+
+function test_decimal ()
+{
+    local TEST
+
+    # Check nothing
+    TEST=$(decimal)
+    echo -n "-$?"
+    [[ "$TEST" -eq 0 ]] && echo -n 1
+
+    # Check int value
+    TEST=$(decimal "${TEST_NUMBER_STR_INT}")
+    echo -n "-$?"
+    [[ "$TEST" -eq 0 ]] && echo -n 1
+
+     # Check string value
+    TEST=$(decimal "${TEST_NUMBER_STR_UNKNOWN}")
+    echo -n "-$?"
+    [[ "$TEST" -eq 0 ]] && echo -n 1
+
+    # Check float value
+    TEST=$(decimal "${TEST_NUMBER_STR_BIGGER_FLOAT}")
+    echo -n "-$?"
+    [[ "$TEST" -eq "${TEST_NUMBER_STR_INT}" ]] && echo -n 1
+
+    # Check float value with decimal starting with zero
+    TEST=$(decimal "${TEST_NUMBER_STR_FLOAT_LEADING_ZERO}")
+    echo -n "-$?"
+    [[ "$TEST" -eq "${TEST_NUMBER_STR_INT}" ]] && echo -n 1
+}
+
+
+readonly TEST_INT="-11-11-01-01-01"
+
+function test_int ()
+{
+    local TEST
+
+    # Check nothing
+    TEST=$(int)
+    echo -n "-$?"
+    [[ "$TEST" -eq 0 ]] && echo -n 1
+
+    # Check bad value
+    TEST=$(int "${TEST_NUMBER_STR_UNKNOWN}")
+    echo -n "-$?"
+    [[ "$TEST" -eq 0 ]] && echo -n 1
+
+    # Check float value
+    TEST=$(int "${TEST_NUMBER_STR_BIG_FLOAT}")
+    echo -n "-$?"
+    [[ "$TEST" -eq "${TEST_NUMBER_STR_INT}" ]] && echo -n 1
+
+    # Check integer value with leading zero
+    TEST=$(int "${TEST_NUMBER_STR_INT_LEADING_ZERO}")
+    echo -n "-$?"
+    [[ "$TEST" -eq "${TEST_NUMBER_STR_INT}" ]] && echo -n 1
+
+    # Check integer value
+    TEST=$(int "${TEST_NUMBER_STR_BIG_INT}")
+    echo -n "-$?"
+    [[ "$TEST" -eq "${TEST_NUMBER_STR_BIG_INT}" ]] && echo -n 1
+}
+
 
 readonly TEST_IS_FLOAT="-11-11-11-01-11"
 
@@ -189,6 +258,34 @@ function test_floatLowerThan ()
 }
 
 
+readonly TEST_FLOAT_FLOOR="-11-11-01-01"
+
+function test_floor ()
+{
+    local TEST
+
+    # Check nothing
+    TEST=$(floor)
+    echo -n "-$?"
+    [[ "$TEST" -eq 0 ]] && echo -n 1
+
+    # Check invalid type
+    TEST=$(floor "${TEST_NUMBER_STR_UNKNOWN}")
+    echo -n "-$?"
+    [[ "$TEST" -eq 0 ]] && echo -n 1
+
+    # Check int value
+    TEST=$(floor "${TEST_NUMBER_STR_BIG_INT}")
+    echo -n "-$?"
+    [[ "$TEST" -eq ${TEST_NUMBER_STR_BIG_INT} ]] && echo -n 1
+
+    # Check float value
+    TEST=$(floor "${TEST_NUMBER_STR_BIG_FLOAT}")
+    echo -n "-$?"
+    [[ "$TEST" -eq ${TEST_NUMBER_STR_INT} ]] && echo -n 1
+}
+
+
 readonly TEST_NUMERIC_TYPE="-11-01-01-01-11"
 
 function test_numericType ()
@@ -222,10 +319,32 @@ function test_numericType ()
 }
 
 
+readonly TEST_FLOAT_RAND="-01-01"
+
+function test_rand ()
+{
+    local TEST NEXT_TEST
+
+    # Check
+    TEST=$(rand)
+    echo -n "-$?"
+    [[ "$TEST" -gt 0 ]] && echo -n 1
+
+    # Another check
+    NEXT_TEST=$(rand)
+    echo -n "-$?"
+    [[ "$NEXT_TEST" -gt 0 && "$TEST" != "$NEXT_TEST" ]] && echo -n 1
+}
+
+
 # Launch all functional tests
+bashUnit "decimal" "${TEST_DECIMAL}" "$(test_decimal)"
+bashUnit "int" "${TEST_INT}" "$(test_int)"
 bashUnit "isFloat" "${TEST_IS_FLOAT}" "$(test_isFloat)"
 bashUnit "isInt" "${TEST_IS_INT}" "$(test_isInt)"
 bashUnit "isNumeric" "${TEST_IS_NUMERIC}" "$(test_isNumeric)"
 bashUnit "floatGreaterThan" "${TEST_FLOAT_GREATER_THAN}" "$(test_floatGreaterThan)"
 bashUnit "floatLowerThan" "${TEST_FLOAT_LOWER_THAN}" "$(test_floatLowerThan)"
+bashUnit "floor" "${TEST_FLOAT_FLOOR}" "$(test_floor)"
 bashUnit "numericType" "${TEST_NUMERIC_TYPE}" "$(test_numericType)"
+bashUnit "rand" "${TEST_FLOAT_RAND}" "$(test_rand)"
